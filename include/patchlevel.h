bcf2cbc9867f4a17eeb25bfeb588c699e467d09c
Sebastian Wiedenroth <wiedi@frubar.net>
2014-10-18 21:42:52 +0200

    Bump patchlevel to 14.10

--- include/patchlevel.h
+++ include/patchlevel.h
@@ -18,7 +18,7 @@
  * $Id: patchlevel.h,v 1.23.2.22 2007/03/09 23:49:31 entrope Exp $
  *
  */
-#define PATCHLEVEL "10+snircd(1.3.4a)+w0nko(13.11)"
+#define PATCHLEVEL "10+snircd(1.3.4a)+w0nko(14.10)"
 
 #define RELEASE ".12."
 
